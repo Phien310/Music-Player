67e81d77a2bb38f6eb444378352d6f61463e0641
Phien <phien301002@gmail.com>
2022-06-10 23:47:24 +0700

    First commit

---
+++ Music Player/assets/base.js
@@ -0,0 +1,32 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const songs = [{
+        name: 'Tình sắc muôn màu',
+        singer: 'Chi xê',
+        path: './assets/List-Song/Tinh-Sac-Muon-Mau-Luong-Bich-Huu.mp3',
+        image: './assets/IMG/maxresdefault.jpg'
+
+    },
+    {
+        name: 'Có hẹn với thanh xuân',
+        singer: 'MONSTART',
+        path: './assets/List-Song/co-hen-voi-thanh-xuan-MONSTAR.mp3',
+        image: './assets/IMG/list-1.jpg'
+
+    },
+    {
+        name: 'Dụi dàng ngày em đến',
+        singer: 'Chi xê',
+        path: './assets/List-Song/Diu-Dang-Em-Den-ERIK-NinjaZ.mp3',
+        image: './assets/IMG/list-2.jpg'
+
+    },
+    {
+        name: 'Xe đạp',
+        singer: 'Chi xê',
+        path: './assets/List-Song/Xe-Dap-Thuy-Chi-M4U.mp3',
+        image: './assets/IMG/list-3.jpg'
+
+    },
+]
